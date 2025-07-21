df1bd7e06e30db88ed4461b9b43e70a1402830b4
Raziya-023 <raziaa0023@gmail.com>
2025-07-21 15:03:15 +0530

    refactor user routes to include additional account management and profile features

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,6 +35,16 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/changePassword").post(verifyJWT, changeCurrentPassword)
+router.route("/currentUser").get(verifyJWT, getCurrentUser)
+router.route("/updateAccountDetails").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get(verifyJWT, getWatchHistory)
+
 
 
 export default router
