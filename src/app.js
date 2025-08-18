7f0d86614d7205d63299fd9afc022ca1862d2dbc
Raziya-023 <raziaa0023@gmail.com>
2025-08-18 16:10:07 +0530

    bug fix

--- src/app.js
+++ src/app.js
@@ -17,10 +17,23 @@ app.use(cookieParser())
 //routes import
 import userRouter from './routes/user.route.js'
 import videoRouter from "./routes/video.route.js"
+import tweetRouter from "./routes/tweet.route.js"
+import subscriptionRouter from "./routes/subscription.route.js"
+import commentRouter from "./routes/comment.route.js"
+import likeRouter from "./routes/like.route.js"
+import playlistRouter from "./routes/playlist.route.js"
+import dashboardRouter from "./routes/dashboard.route.js"
+
 
 //route declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
 
